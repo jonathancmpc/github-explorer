ecc98ef2987605b0b8a5ca8419834ae526a0975f
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-03-29 08:05:18 -0300

    Initial TypeScript settings

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
+    '@babel/preset-typescript',
     ['@babel/preset-react', {
       runtime: 'automatic' /* Agora não precisamos chmar o React em todo arquivo que utilizamos o jsx */
     }]
